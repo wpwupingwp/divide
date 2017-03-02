110ba9b8bd9c6928f7cb984022237de225fca4db
Ping Wu <wpwupingwp@outlook.com>
2017-03-02 09:35:19 +0800

    Moved from github.com/wpwupingwp/python.git

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # divide
-Divide NGS data by barcode and primer
+
+## divide.py
+
+Divide NGS data by barcode and primer.
+
+Make sure you don't miss the first line.
+Barcode file looks like this:
+
+>    barcode,sample
+
+>    ATACG,BOP00001
+
+Primer file looks like this:
+
+>    gene,primer,sequence,direction
+
+>    rbcL,rbcLF,ATCGATCGATCGA,f
+
+>    rbcL,rbcLR,TACGTACGTACG,r
---
+++ task.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#PBS -N merge
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=100:00:00 
+#PBS -q batch             
+#PBS -S /bin/bash        
+
+Workdir=/home/user/data/
+cd $Workdir
+L=$Workdir/L.fastq
+R=$Workdir/R.fastq
+BarcodeFile=$Workdir/barcode.csv
+PrimerFile=$Workdir/primer.csv
+Merged=$Workdir/merged.fastq
+cd $Workdir
+time flash -t 8 -M 200 $L $R
+time python3 join_fastq.py out.notCombined_1.fastq out.notCombined_2.fastq
+cat out.extendedFrags.fastq combine.fastq > $Merged
+time python3 divide.py $Merged -b $BarcodeFile -p PrimerFile
