751ba9e5e90a932dc40d0cf302224e28ea9f1885
Workstation <wpwupingwp@outlook.com>
2017-05-01 20:00:09 +0800

    check flash

--- call_flash2.py
+++ call_flash2.py
@@ -1,4 +1,15 @@
 from subprocess import call
 
+
+def check_flash():
+    found = 0
+    for program in ('flash2', 'flash'):
+        check = call('{0} --version'.format(program), shell=True)
+        if check.returncode == 0:
+            return program
+    if found == 0:
+        raise Exception('flash not found, please install it!')
+
+
 call('flash2 {0} {1} -o {2} -d {3}'.format(
     F, R, arg.out_prefix, arg.out_directory) , shell=True)
